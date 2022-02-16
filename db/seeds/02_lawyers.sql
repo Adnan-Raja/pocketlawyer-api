babd5e42095536f82a026f8da37ad23869d48826
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-02-16 17:07:54 -0500

    update seed data

--- db/seeds/02_lawyers.sql
+++ db/seeds/02_lawyers.sql
@@ -6,7 +6,7 @@
 
 
 
-INSERT INTO lawyers (id, name, img) VALUES (1, 'Harvey Specter', 'https://static.wikia.nocookie.net/suits/images/d/da/S8_Harvey_Specter.jpg/revision/latest?cb=20180731195449' );
-INSERT INTO lawyers (id, name, img) VALUES (2, 'Michael Ross', 'https://static.wikia.nocookie.net/suits/images/c/c2/Mike_Ross_%285x7%29.png/revision/latest?cb=20190903001426' );
+INSERT INTO lawyers (id, name, img) VALUES (1, 'Harvey Specter', 'https://pbs.twimg.com/profile_images/1302718446724100096/MINdq-8q_400x400.jpg' );
+INSERT INTO lawyers (id, name, img) VALUES (2, 'Michael Ross', 'https://heroandvillainstyle.com/wp-content/uploads/2020/09/Mike-Ross-Slicked-Back-Hair-e1599847799376.jpg' );
 INSERT INTO lawyers (id, name, img) VALUES (3, 'Louis Litt', 'https://static.tvmaze.com/uploads/images/medium_portrait/161/404013.jpg' );
 
