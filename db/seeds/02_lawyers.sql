cb69bd11a0429e45c5128be9f629445f1484c25a
Adnan-Raja <adnan.saeed.raja@gmail.com>
2022-02-16 23:36:21 -0500

    update lawyer schema and seeds

--- db/seeds/02_lawyers.sql
+++ db/seeds/02_lawyers.sql
@@ -1,12 +1,14 @@
 -- CREATE TABLE lawyers (
 --   id SERIAL PRIMARY KEY NOT NULL,
 --   name VARCHAR(255) NOT NULL,
---   img text
+--   img text,
+--   description text NOT NULL;
+--   focus VARCHAR(255) NOT NULL
 -- );
 
 
 
-INSERT INTO lawyers (id, name, img) VALUES (1, 'Harvey Specter', 'https://pbs.twimg.com/profile_images/1302718446724100096/MINdq-8q_400x400.jpg' );
-INSERT INTO lawyers (id, name, img) VALUES (2, 'Michael Ross', 'https://heroandvillainstyle.com/wp-content/uploads/2020/09/Mike-Ross-Slicked-Back-Hair-e1599847799376.jpg' );
-INSERT INTO lawyers (id, name, img) VALUES (3, 'Louis Litt', 'https://static.tvmaze.com/uploads/images/medium_portrait/161/404013.jpg' );
-
+INSERT INTO lawyers (id, name, img, description, focus) VALUES (1, 'Harvey Specter', 'https://pbs.twimg.com/profile_images/1302718446724100096/MINdq-8q_400x400.jpg','Maritime law deals with all manner of legal disputes in the water. Admiralty attorneys may assist companies with nautical issues like domestic and foreign shipping routes, naval accidents, even maritime piracy. Maritime laws extend to American waters and any waters navigable by US vessels, so these attorneys will need extensive international legal knowledge.','Maritime Lawyer');
+INSERT INTO lawyers (id, name, img, description, focus) VALUES (2, 'Michael Ross', 'https://heroandvillainstyle.com/wp-content/uploads/2020/09/Mike-Ross-Slicked-Back-Hair-e1599847799376.jpg','If a company or individual is insolvent, it is a bankruptcy lawyers job to advise them about their eligibility for bankruptcy, prepare their legal paperwork, and represent them in bankruptcy court. These lawyers advise clients when to file for bankruptcy and what type of bankruptcy to pursue in the clients best interests. They also present their clients with legal alternatives to filing for bankruptcy, where such exists.','Bankruptcy Lawyer');
+INSERT INTO lawyers (id, name, img, description, focus) VALUES (3, 'Louis Litt', 'https://static.tvmaze.com/uploads/images/medium_portrait/161/404013.jpg','Civil litigation attorneys help clients in all matters of civil lawsuits, but not government disputes. The lawyer may represent either the defendant or the plaintiff and can further specialize in commercial litigation, corporate litigation, or environmental litigation.','Civil Litigation Lawyer');
+INSERT INTO lawyers (id, name, img, description, focus) VALUES (4, 'James Oliver', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE1d0YWy8kf2dflPnFjgk-Pl_AP_SaeCGm7g&usqp=CAU.jpg','Civil rights lawyers handle cases involving charging the state or other authorities with violations of an individuals constitutional rights. Circumstances may include harassment, civil liberties, voting rights, human rights, or discrimination based on sex, race, age, religion, gender, national origin, physical or mental disability, sexual orientation, and more. In addition, civil rights lawyers secure the rights of individuals to the freedoms of expression, speech, movement, privacy, thought, religion, and the press.','Civil Rights Lawyer');
