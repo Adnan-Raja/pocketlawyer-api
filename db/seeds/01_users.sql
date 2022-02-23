c7834fa41a606325717cdc76bf090c932e0f4ab6
Leshan <leshan0627@gmail.com>
2022-02-23 01:56:04 -0500

    chore: update to database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (id, username, img, password) VALUES (1, 'Alice', 'al@gmail.com', 'password');
-INSERT INTO users (id, username, img, password) VALUES (2, 'Kira', 'kp@gmail.com', 'password');
+INSERT INTO users (id, name, img, password) VALUES (1, 'Alice', 'al@gmail.com', 'password');
+INSERT INTO users (id, name, img, password) VALUES (2, 'Kira', 'kp@gmail.com', 'password');
+INSERT INTO users (id, name, img, password) VALUES (3, 'Walter', 'https://tse4.mm.bing.net/th?id=OIP.I0rRw6lUEdvIGZ7sE54jMQHaDt&pid=Api&P=0&w=349&h=174', "password");
+INSERT INTO users (id, name, img, password) VALUES (4, 'Jessica', 'https://topicimages.mrowl.com/large/greensquall/suitstv/characters/jessicapearson_1.jpg', "password");
